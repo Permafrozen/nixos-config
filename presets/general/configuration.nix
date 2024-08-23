376a85341cd56c7c0e18c7f3f1b79aecdc2dc854
Matteo <m8tt8o@gmail.com>
2024-08-23 15:26:33 +0200

    Added ssh support

--- presets/general/configuration.nix
+++ presets/general/configuration.nix
@@ -81,6 +81,8 @@
     proggyfonts
   ];
 
+  services.openssh.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
